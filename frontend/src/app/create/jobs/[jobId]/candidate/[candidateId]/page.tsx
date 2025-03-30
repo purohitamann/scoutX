7331620d0106b4294245205b69194ab2c35fd6c3
Aman Purohit <122639076+purohitamann@users.noreply.github.com>
2025-03-30 03:40:44 -0400

    profile edit

--- frontend/src/app/create/jobs/[jobId]/candidate/[candidateId]/page.tsx
+++ frontend/src/app/create/jobs/[jobId]/candidate/[candidateId]/page.tsx
@@ -7,7 +7,7 @@ import { use } from 'react';
 export default function CandidatePage({ params }: { params: Promise<{ candidateId: string }> }) {
   const { candidateId } = use(params);
   return (
-    <div className="">
+    <div className="container mx-auto p-4 bg-gradient-to-b from-gray-900 to-gray-800">
       <CandidateProfile candidateId={candidateId} />
     </div>
   );
