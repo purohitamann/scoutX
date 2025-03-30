bbabd16cfe3f90182a8d36a3d13c3fc3d88c163b
Patel Parth <parth.patel0774@gmail.com>
2025-03-30 02:05:37 -0400

    fixing agent

---
+++ backend/Test/utils/twilio_config/twilio_config.py
@@ -0,0 +1,4 @@
+twilioAccountSid = "AC8249b49b775707de422a623d2da30c27"
+twilioAuthToken = "a59ddef0197ce533de085f11a95f51f1"
+twilioPhoneNumber = "+19143346292"
+phoneNumberId = "2840de11-bb64-4d66-b58b-b919ec2ac0e4"
